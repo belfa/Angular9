0e7080a00e557b1da353dc25ceb6a18e9b0a8c0e
Miguel Patiño <miguel36410@gmail.com>
2020-05-07 15:19:31 +0200

    Binding data

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -2,15 +2,23 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  template: `<app-server></app-server>
-  <app-server></app-server>`,
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-
-  constructor() { }
+  allowNewServer = false;
+  serverCreationStatus = "No server was created!";
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = !this.allowNewServer;
+    }, 5000);
+  }
 
   ngOnInit(): void {
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created!';
   }
 
 }
